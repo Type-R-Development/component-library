f04fbd2cb3179d5103cc472d5521e54491c94c0c
webdevtrev <66264441+webdevtrev@users.noreply.github.com>
2024-02-03 16:22:16 -0500

    Changes to storybook build and publish

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,21 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
-  docs: {
-    autodocs: "tag",
+  async viteFinal(config, options) {
+    // Add your configuration here
+    // config.server = { fs: { strict: false } };
+    // config.build = { assetsDir: 'test' };
+    config.base = 'https://type-r-development.github.io/component-library/';
+    return config;
   },
 };
 export default config;
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-export { default as Button } from 'src/BaseComponents/Button';
+export { default as Button } from './src/BaseComponents/Button';
