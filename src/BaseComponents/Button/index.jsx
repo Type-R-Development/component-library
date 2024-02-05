942d0d6da2c09465e522dc726e1446b633c662a5
webdevtrev <66264441+webdevtrev@users.noreply.github.com>
2024-02-04 19:38:58 -0500

    Adds Wrapper prop to buttons and configures prop types and storybook control

--- src/BaseComponents/Button/index.jsx
+++ src/BaseComponents/Button/index.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 
-export default function Button({ className, children, ...rest }) {
+export default function Button({
+  Wrapper = 'button',
+  className,
+  children,
+  ...rest
+}) {
   return (
-    <button className={`Button-Container ${className ?? ''}`} {...rest}>
+    <Wrapper className={`Button-Container ${className ?? ''}`} {...rest}>
       {children}
-    </button>
+    </Wrapper>
   );
 }
 Button.PropTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  Wrapper: PropTypes.string,
 };
